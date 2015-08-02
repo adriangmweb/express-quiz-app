815c6c1a43f5f5099da713eab1a25c95eaddde2b
Adrian GM <adrian@iMac.local>
2015-08-02 12:44:30 +0100

    Despliegue DB en heroku

--- controllers/quiz_controller.js
+++ controllers/quiz_controller.js
@@ -1,14 +1,19 @@
+var models = require('../models/models.js');
 //Get /quizes/question
 exports.question = function(req, res){
-  res.render('quizes/question', {pregunta: '¿Cuál es la capital de Italia?', pista: 'No olvides usar mayúsculas.'});
+  models.Quiz.findAll().then(function(quiz) {
+    console.log(quiz);
+    res.render('quizes/question', {pregunta: quiz[0].pregunta, pista: quiz[0].pista});
+  });
 };
 
 //GET /quizes/answer
 exports.answer = function(req, res){
-  console.log(req.query.respuesta);
-  if (req.body.respuesta === 'Roma'){
-    res.render('quizes/answer', {respuesta: 'Correcta'});
-  }else{
-    res.render('quizes/answer', {respuesta: 'Incorrecta'});
-  }
+  models.Quiz.findAll().success(function(quiz) {
+    if (req.body.respuesta === quiz[0].respuesta){
+      res.render('quizes/answer', {respuesta: 'Correcta'});
+    }else{
+      res.render('quizes/answer', {respuesta: 'Incorrecta'});
+    }
+  });
 };
---
+++ models/models.js
@@ -0,0 +1,46 @@
+var path = require('path');
+
+//Postgres DATABASE_URL = postgres://user:passwd@host:port/database
+//SQLite DATABASE_URL = sqlite://:@:/
+var url = process.env.DATABASE_URL.match(/(.*)\:\/\/(.*?)\:(.*)@(.*)\:(.*)\/(.*)/);
+var DB_name = (url[6]||null);
+var user = (url[2]||null);
+var pass_db = (url[3]||null);
+var protocol = (url[1]||null);
+var dialect = (url[1]||null);
+var port = (url[5]||null);
+var host = (url[4]||null);
+var storage = process.env.DATABASE_STORAGE;
+
+//Cargar ORM
+var Sequelize = require("sequelize");
+
+//Usar DB SQLite o Postgres
+var sequelize = new Sequelize(DB_name, user, pass_db, {
+                      dialect: dialect,
+                      protocol: protocol,
+                      port: port,
+                      host: host,
+                      storage: storage, //only sqlite
+                      omitNull: true //only postgres
+                    }
+                    );
+//Importar referencia a la tabla Quiz en quiz.js
+var Quiz = sequelize.import(path.join(__dirname, 'quiz'));
+
+exports.Quiz = Quiz; //Exportar la referencia a la tabla
+
+//Sincronizamos y creamos la tabla "preguntas"
+sequelize.sync().then(function() {
+  //then se ejecuta una vez se cree la tabla
+  Quiz.count().then(function (count) {
+    if (count === 0){ //Si está vacía se inicializa la tabla
+      Quiz.create({
+        pregunta: '¿Cuál es la capital de Italia?',
+        respuesta: 'Roma',
+        pista: "No olvides las mayúsculas"
+      })
+      .then(function(){console.log('La base de datos se ha inicializado')});
+    };
+  });
+});
